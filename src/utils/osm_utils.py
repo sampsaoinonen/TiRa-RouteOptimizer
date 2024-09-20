85582f978472359629603471d059fcbb048b1b93
Sampsa Oinonen <sampsa.oinonen@gmail.com>
2024-09-20 17:16:17 +0300

    Update osm_utils.py docstring

--- src/utils/osm_utils.py
+++ src/utils/osm_utils.py
@@ -1,22 +1,27 @@
 import osmnx as ox
 
 def download_osm_graph(place_name, network_type='drive'):
-    """
-    Downloads the OSMnx graph for the specified location.
-    
-    :param place_name: Name of the place to download the graph for (e.g., 'Helsinki, Finland').
-    :param network_type: The type of network to download (e.g., 'drive' for car-accessible roads).
-    :return: The downloaded NetworkX graph.
+    """ Downloads the OSMnx graph for the specified location.
+
+    Args:
+        place_name (str): Name of the place to download the graph for (e.g., 'Helsinki, Finland').
+        network_type (str): The type of network to download (e.g., 'drive' for car-accessible roads). 
+                            Defaults to 'drive'.
+
+    Returns:
+        networkx.Graph: The downloaded OSMnx graph.
     """
     return ox.graph_from_place(place_name, network_type=network_type)
 
 def get_nearest_node(graph, lat, lon):
-    """
-    Gets the nearest node in the OSMnx graph to the specified latitude and longitude.
-    
-    :param graph: The OSMnx graph.
-    :param lat: Latitude of the point.
-    :param lon: Longitude of the point.
-    :return: The nearest node in the graph.
+    """ Gets the nearest node in the OSMnx graph to the specified latitude and longitude.
+
+    Args:
+        graph (networkx.Graph): The OSMnx graph.
+        lat (float): Latitude of the point.
+        lon (float): Longitude of the point.
+
+    Returns:
+        int: The nearest node in the graph.
     """
     return ox.distance.nearest_nodes(graph, X=lon, Y=lat)
