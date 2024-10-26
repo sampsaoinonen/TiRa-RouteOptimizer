f90da7132ffdad1eb3e4ff97ac1b845f391921c6
Sampsa Oinonen <sampsa.oinonen@gmail.com>
2024-10-26 14:21:25 +0300

    Update tasks.py commands to display test print outputs in the terminal

--- tasks.py
+++ tasks.py
@@ -3,35 +3,34 @@ from invoke import task
 @task
 def start(c):
     """Run the app."""
-    c.run("poetry run pytest -s src/tests/unit")
+    c.run("poetry run coverage run --branch -m pytest -s src/tests/unit", pty=True)
     c.run("poetry run python src/app.py")
 
 @task
 def all_tests(c):
     """Run all tests including performance tests."""
-    c.run("poetry run coverage run --branch -s pytest src/tests")
+    c.run("poetry run coverage run --branch -m pytest -s src/tests", pty=True)
 
 @task
 def tests(c):
     """Run the tests excluding performance tests."""
-    c.run("poetry run coverage run --branch -m pytest src/tests -k 'not performance'")
+    c.run("poetry run coverage run --branch -m pytest -s src/tests -k 'not performance'", pty=True)
 
 @task
 def unit_tests(c):
     """Run the unit tests."""
-    c.run("poetry run coverage run --branch -m pytest src/tests/unit")
+    c.run("poetry run coverage run --branch -m pytest -s src/tests/unit", pty=True)
 
 @task
 def integration_tests(c):
     """Run the integration tests."""
-    c.run("poetry run coverage run --branch -m pytest src/tests/integration")
+    c.run("poetry run coverage run --branch -m pytest -s src/tests/integration", pty=True)
     
 @task
 def performance_tests(c):
     """Run performance tests, save plot and performance-log in /test-results"""
-    c.run("mkdir -p test-results") # Create test-results directory if it doesn't exist
-    c.run("poetry run pytest -s src/tests/performance | tee test-results/performance-log.txt", pty=True)
-
+    c.run("mkdir -p test-results") # Create test-results directory if not exist
+    c.run("poetry run coverage run --branch -m pytest -s src/tests/performance | tee test-results/performance-log.txt", pty=True)
 @task
 def lint(c):
     """Run Pylint with a min 9.8/10 rating."""
